be3c146fa7bc71ca9a4c2bece28ef1779d4322fe
Ryan Kayu Ching <ryanching@hotmail.com>
2017-01-01 00:51:26 -0800

    Simple styling

--- routes/app.js
+++ routes/app.js
@@ -3,25 +3,9 @@ var router = express.Router();
 var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    User.findOne({}, function(err, doc){
-      if (err) {
-        return res.send("Error!");
-      } else {
-        res.render('node', {email: doc.email});
-      }
-    });
-});
 
-router.post('/', function (req, res, next) {
-  var email = req.body.email;
-  var user = new User({
-    firstName: 'Ryan',
-    lastName: 'Ching',
-    password: 'supersecret',
-    email: email
-  });
-  user.save();
-  res.redirect('/');
+  res.render('index');
+
 });
 
 module.exports = router;
