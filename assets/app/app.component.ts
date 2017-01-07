c42c645a68e8cb83737d6e4ab23d49601db2352a
Ryan Kayu Ching <ryanching@hotmail.com>
2017-01-06 16:16:58 -0800

    Refactor components and implement Routing

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
+import { Message } from "./messages/message.model";
+import { MessageService } from "./messages/message.service";
+
 @Component({
     selector: 'my-app',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+    providers: [MessageService]
 })
 export class AppComponent {
-  message = {
-    content: "A message",
-    author: "Ryan"
-  }
+
 }
